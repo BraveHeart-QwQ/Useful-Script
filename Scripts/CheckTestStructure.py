7f73f3a4607dcdf352f809c7ef3933fcbdf83506
Hanie <holdonabraveheart_qwq@outlook.com>
2023-07-23 22:25:23 +0800

    Fix bug

--- Scripts/CheckTestStructure.py
+++ Scripts/CheckTestStructure.py
@@ -2,16 +2,27 @@ import sys
 import os
 import json
 
+_PROJECT_DIR = "."
 _DEFAULT_SOURCE_DIR = "src"
 _DEFAULT_TEST_DIR = "tests"
+_IGNORE_FILE = []
+
+
+def _Init():
+    global _IGNORE_FILE
+    with open(os.path.join(_PROJECT_DIR, 'ignore-tests.json')) as file_:
+        _IGNORE_FILE = json.load(file_)['ignoreTest']
+
+
+def _IsIgnore(file_: str) -> bool:
+    for ignoreFile in _IGNORE_FILE:
+        if (os.path.samefile(ignoreFile, file_)): return True
+    return False
 
 
 def _CheckTestStructure(srcDir=_DEFAULT_SOURCE_DIR, testDir=_DEFAULT_TEST_DIR) -> bool:
-    projectDir = os.path.abspath(".")
-    with open(os.path.join(projectDir, 'ignore-tests.json')) as file_:
-        ignoreList = json.load(file_)['ignoreTest']
-
     rightStructure = True
+    projectDir = os.path.abspath(".")
     srcDir = os.path.join(projectDir, srcDir)
     testDir = os.path.join(projectDir, testDir)
 
@@ -27,9 +38,9 @@ def _CheckTestStructure(srcDir=_DEFAULT_SOURCE_DIR, testDir=_DEFAULT_TEST_DIR) -
             relaDir = os.path.relpath(root, srcDir)
             testFile = os.path.join(testDir, relaDir, testFilename)
 
-            if not os.path.exists(testFile) and projectFile not in ignoreList:
+            if (not os.path.exists(testFile) and not _IsIgnore(projectFile)):
                 rightStructure = False
-                print(f'Missing tests: {relaPath}')
+                print(f'Missing test: {relaPath}')
 
     print("==========TESTS==========")
     for root, dirs, files in os.walk(testDir):
@@ -41,7 +52,7 @@ def _CheckTestStructure(srcDir=_DEFAULT_SOURCE_DIR, testDir=_DEFAULT_TEST_DIR) -
             relaDir = os.path.relpath(root, testDir)
 
             if (not file_.endswith("Test.cs")):
-                print(f"Not test file: {relaPath}")
+                print(f"Non-test file: {relaPath}")
                 continue
 
             projectFilename = file_[:-7] + ".cs"
@@ -55,6 +66,7 @@ def _CheckTestStructure(srcDir=_DEFAULT_SOURCE_DIR, testDir=_DEFAULT_TEST_DIR) -
 
 
 def Main():
+    _Init()
     argv = sys.argv[1:]
     if (len(argv) == 0):
         _CheckTestStructure()
